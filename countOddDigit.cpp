8718fed82a1b25b14d18cdbcf49f3608f98d2110
Atul Kumar Vishwakarma <vishwakarmaatulkumar388@gmail.com>
2025-09-28 23:03:48 +0530

    Create countOddDigit.cpp

---
+++ countOddDigit.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countOddDigit(int n) {
+        int count = 0;
+        if(n==0) return 0;
+        while(n!=0){
+            int digit = n%10;
+            if(digit%2 != 0) count ++;
+            n = n/10;
+        }
+        return count;
+    }
+};
