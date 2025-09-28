f13643b007314db6d816b427f60f3eb781c07ac7
Atul Kumar Vishwakarma <vishwakarmaatulkumar388@gmail.com>
2025-09-28 23:00:53 +0530

    Create countDigitOfNum.cpp

---
+++ countDigitOfNum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countDigit(int n) {
+        int count = 0;
+        if(n==0) return 1;
+        while(n !=0){
+            int digit = n%10;
+            n = n/10;
+            count ++;
+        }
+        return count;
+    }
+};
